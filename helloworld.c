cb1d0dd4dacbff242779a1f7498625d2876c7d4a
A00344428 <vane_750122@hotmail.com>
2021-02-17 03:15:28 +0000

    setup

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 
-int main (){
-	printf("Hello world\n");
+/*int main (int numarg, char **argv){
+	printf("Hello world %s\n", argv[1]);
 	
 	return 0;
+}*/
+
+int main (int numarg, char **argv){
+	
+	for(int i = 1; i<numarg; i++){
+		printf("Hello world %s\n", argv[i]);
+	}
 }
