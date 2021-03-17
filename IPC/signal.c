9789acc7f8744ecc45a31621014dfda53558b93b
A00344428 <vane_750122@hotmail.com>
2021-03-17 02:57:38 +0000

    Practica 3

---
+++ IPC/signal.c
@@ -0,0 +1,22 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+int ret = 1;
+
+void signalHandler(int sig){
+	printf("\nRecibí señal %d\n", sig);
+	if(sig == 2){
+		ret = 0;
+	}
+}
+
+int main(){
+	signal(2, signalHandler); // CNTRL + C
+	signal(15,signalHandler);
+	while(ret){
+		printf("Trabajando\n");
+		sleep(1);
+	}
+	printf("Ya voy a terminar\n ");
+	return 0;
+}
