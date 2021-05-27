6d12b833e8733efcb8b98871133f7865a9691ddf
vanessa2213 <vane_750122@hotmail.com>
2021-05-27 01:06:44 +0000

    Practica 13

---
+++ files/search.c
@@ -0,0 +1,54 @@
+#include "student.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+    char *filename = argv[1];
+    char *type_newstudent = argv[2];
+    char *data = argv[3];
+
+    FILE *forigin;
+    forigin = fopen(filename,"rb");
+
+    STUDENT newstudent;
+    
+    while(fread(&newstudent, sizeof(newstudent),1,forigin) ){
+            if(strcmp(type_newstudent,"name")==0){
+                if(strcmp(newstudent.name, data)==0){
+                    printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
+                }   
+            }
+            else if (strcmp(type_newstudent,"lastname")==0){
+
+                if(strcmp(newstudent.lastname, data)==0){
+                    printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
+                };
+            }  
+            else if(strcmp(type_newstudent,"id")==0){
+                char st[20];
+                sprintf(st, "%d", newstudent.id);
+                //printf("%s & %s \n", st, data);
+
+                if(strcmp(st, data)==0){
+                    printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
+                }
+            } 
+            else if(strcmp(type_newstudent,"semestre")==0){
+                char st[20];
+                sprintf(st, "%d", newstudent.semestre);
+                
+                if(strcmp(st,data)==0){
+                    printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
+                }
+            } 
+            else{
+                printf("n");
+            }
+    }
+
+    
+    fclose(forigin);
+
+}
