ecb0aa3b082a02371297c3a05ab6a93c9b2cc128
vanessa2213 <vane_750122@hotmail.com>
2021-05-31 19:42:35 +0000

    Practica 13

--- files/search.c
+++ files/search.c
@@ -8,23 +8,31 @@ int main(int argc, char **argv) {
     char *filename = argv[1];
     char *type_newstudent = argv[2];
     char *data = argv[3];
-
+    int i = 0;
     FILE *forigin;
     forigin = fopen(filename,"rb");
 
     STUDENT newstudent;
     
     while(fread(&newstudent, sizeof(newstudent),1,forigin) ){
+        i++;
             if(strcmp(type_newstudent,"name")==0){
                 if(strcmp(newstudent.name, data)==0){
                     printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
                 }   
+                else{
+                    printf("Error: no se encontró el nombre en el alumno %d\n", i);
+                }
             }
             else if (strcmp(type_newstudent,"lastname")==0){
 
                 if(strcmp(newstudent.lastname, data)==0){
                     printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
-                };
+                }
+                else{
+                    printf("Error: no se encontró el apellido en el alumno %d\n", i);
+                }
+                
             }  
             else if(strcmp(type_newstudent,"id")==0){
                 char st[20];
@@ -34,6 +42,9 @@ int main(int argc, char **argv) {
                 if(strcmp(st, data)==0){
                     printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
                 }
+                else{
+                    printf("Error: no se encontró el semestre en el alumno %d\n", i);
+                }
             } 
             else if(strcmp(type_newstudent,"semestre")==0){
                 char st[20];
@@ -42,9 +53,13 @@ int main(int argc, char **argv) {
                 if(strcmp(st,data)==0){
                     printf("%s %s %d %d \n", newstudent.name,newstudent.lastname,newstudent.id,newstudent.semestre);
                 }
+                else{
+                    printf("Error: no se encontró el semestre en el alumno %d\n", i);
+                }
             } 
             else{
-                printf("n");
+                printf("Error: no se encontró el atributo\n");
+                break;
             }
     }
 
